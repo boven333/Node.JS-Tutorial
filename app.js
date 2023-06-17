24066d4bf2c8ceee4953806e41d50d3bccfdc1e0
boven333 <112691124+boven333@users.noreply.github.com>
2023-06-17 15:36:34 +0700

    Add all file
    Hi :100:

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+let chalk;
+import('chalk').then((chalkModule) => {
+  chalk = chalkModule.default;
+
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) => {
+    res.send("Hello Kuay");
+});
+
+app.listen(port, ()=>{
+    debug("Listening on port" + chalk.green(" : " + port));
+});
+}).catch((error) => {
+    console.error('Failed to load chalk module:', error);
+  });
